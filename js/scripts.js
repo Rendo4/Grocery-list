1a1e25f4260936391267fe864cb8cd9983ebe5b1
Ryan Rendon <Rendon.S.Ryan@gmail.com>
2021-06-20 15:03:35 -0700

    now create a new array in alaphabetical order and uppercase

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,21 @@
 $(document).ready(function() {
+  let shoppingList = [];
+  $("#info").submit(function(event) {
+    const item = $("#item").val();
+    shoppingList.push(item);
+    shoppingList.sort();
+    
+    event.preventDefault();
+  });
+
   $("p").click(function(event) {
-    event.preventDefault
-    const iceCream = ["vanilla", "chocolate", "mint-chocolate", "rocky road", "cookie dough"]
-    iceCream.forEach(function(flavor) {
-      $("#list").append(`<li> ${flavor} `);
+    let upperList = shoppingList.map(function(item) {
+      return item.toUpperCase();
     });
+    upperList.forEach(function(item) {
+    $("#list").append(`<li> ${item} </li>`);
+    event.preventDefault();
+    });
+    shoppingList = [];
   });
 });
